6d2381fa169de107ecb98c1467353448866e0c08
PredatorDevs <sanchez.gustavo0505@gmail.com>
2021-11-28 22:24:39 -0600

    Role and Case controller added

--- src/controllers/CaseCnt.java
+++ src/controllers/CaseCnt.java
@@ -14,22 +14,22 @@ import java.sql.Statement;
 
 import java.util.ArrayList;
 
-import models.Role;
+import models.Case;
 import services.DatabaseConnection;
 
 /**
  *
  * @author PredatorDev
  */
-public class RoleCnt {
-    public ArrayList<Role> find() throws SQLException {
+public class CaseCnt {
+    public ArrayList<Case> find() throws SQLException {
         DatabaseConnection db = new DatabaseConnection();
-        ArrayList<Role> result = new ArrayList<>();
+        ArrayList<Case> result = new ArrayList<>();
         try (Connection con = db.getConn()) {
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Roles;");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Cases;");
             while(rs.next()) {
-                Role row = new Role(rs.getInt(1), rs.getString(2));
+                Case row = new Case(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getInt(4), rs.getInt(5));
                 result.add(row);
             }
             con.close();
@@ -37,43 +37,49 @@ public class RoleCnt {
         return result.isEmpty() ? null : result;
     }
     
-    public Role findOne(int Id) throws SQLException {
+    public Case findOne(int Id) throws SQLException {
         DatabaseConnection db = new DatabaseConnection();
-        Role result = new Role(0, "");
+        Case result = new Case(0, 0, 0, 0, 0);
         try (Connection con = db.getConn()) {
-            PreparedStatement stmt = (PreparedStatement) con.prepareStatement("SELECT * FROM Roles WHERE Id = ?;");
+            PreparedStatement stmt = (PreparedStatement) con.prepareStatement("SELECT * FROM Cases WHERE Id = ?;");
             stmt.setInt(1, Id);
             ResultSet rs = stmt.executeQuery();
             while(rs.next()) {
-                result = new Role(rs.getInt(1), rs.getString(2));
+                result = new Case(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getInt(4), rs.getInt(5));
             }
             con.close();
         }
         return result;
     }
     
-    public void add(Role data) throws SQLException {
+    public void add(Case data) throws SQLException {
         DatabaseConnection db = new DatabaseConnection();
         try (Connection con = db.getConn()) {
-            PreparedStatement stmt = (PreparedStatement) con.prepareStatement("INSERT INTO Roles (Name) VALUES (?);");
-            stmt.setString(1, data.getName());
+            PreparedStatement stmt = (PreparedStatement) con.prepareStatement("INSERT INTO Cases (PatientId, HospitalId, IsActive, IsDeleted) VALUES (?, ?, ?, ?);");
+            stmt.setInt(1, data.getPatientId());
+            stmt.setInt(2, data.getHospitalId());
+            stmt.setInt(3, data.getIsActive());
+            stmt.setInt(4, data.getIsDeleted());
             int operation = stmt.executeUpdate();
             con.close();
-            System.out.println("Rol añadido correctamente! " + operation + " registros afectados.");
+            System.out.println("Caso añadido correctamente! " + operation + " registros afectados.");
         } catch (Exception e) {
             System.err.println(e);
         }
     }
     
-    public void update(int Id, Role data) throws SQLException {
+    public void update(int Id, Case data) throws SQLException {
         DatabaseConnection db = new DatabaseConnection();
         try (Connection con = db.getConn()) {
-            PreparedStatement stmt = (PreparedStatement) con.prepareStatement("UPDATE Roles SET Name = ? WHERE Id = ?;");
-            stmt.setString(1, data.getName());
-            stmt.setInt(2, Id);
+            PreparedStatement stmt = (PreparedStatement) con.prepareStatement("UPDATE Roles SET PatientId = ?, HospitalId = ?, IsActive = ?, IsDeleted = ? WHERE Id = ?;");
+            stmt.setInt(1, data.getPatientId());
+            stmt.setInt(2, data.getHospitalId());
+            stmt.setInt(3, data.getIsActive());
+            stmt.setInt(4, data.getIsDeleted());
+            stmt.setInt(5, Id);
             int operation = stmt.executeUpdate();
             con.close();
-            System.out.println("Rol actualizado correctamente! " + operation + " registros afectados.");
+            System.out.println("Caso actualizado correctamente! " + operation + " registros afectados.");
         } catch (Exception e) {
             System.err.println(e);
         }
@@ -82,11 +88,11 @@ public class RoleCnt {
     public void delete(int Id) throws SQLException {
         DatabaseConnection db = new DatabaseConnection();
         try (Connection con = db.getConn()) {
-            PreparedStatement stmt = (PreparedStatement) con.prepareStatement("DELETE FROM Roles WHERE Id = ?;");
+            PreparedStatement stmt = (PreparedStatement) con.prepareStatement("DELETE FROM Cases WHERE Id = ?;");
             stmt.setInt(1, Id);
             int operation = stmt.executeUpdate();
             con.close();
-            System.out.println("Rol eliminado correctamente! " + operation + " registros afectados.");
+            System.out.println("Caso eliminado correctamente! " + operation + " registros afectados.");
         } catch (Exception e) {
             System.err.println(e);
         }
--- src/covidapp/CovidApp.java
+++ src/covidapp/CovidApp.java
@@ -30,37 +30,37 @@ public class CovidApp {
     public static void main(String[] args) throws SQLException {
         RoleCnt roleCnt = new RoleCnt();
         Role result = roleCnt.findOne(2);
+        roleCnt.delete(5);
         System.out.println(result.getId());
         System.out.println(result.getName());
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new login().setVisible(true);
-            }
-        });
+//        /* Set the Nimbus look and feel */
+//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+//         */
+//        try {
+//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//                if ("Nimbus".equals(info.getName())) {
+//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        } catch (ClassNotFoundException ex) {
+//            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (InstantiationException ex) {
+//            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (IllegalAccessException ex) {
+//            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        }
+//        //</editor-fold>
+//
+//        /* Create and display the form */
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                new login().setVisible(true);
+//            }
+//        });
     }
-    
 }
