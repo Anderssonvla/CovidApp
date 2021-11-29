448c9d7269c715ab2113324cd76469f3362e4322
PredatorDevs <sanchez.gustavo0505@gmail.com>
2021-11-29 11:38:58 -0600

    UI Init

--- src/covidapp/CovidApp.java
+++ src/covidapp/CovidApp.java
@@ -8,6 +8,7 @@ package covidapp;
 import controllers.RoleCnt;
 import java.sql.SQLException;
 import models.Role;
+import screens.login;
 
 
 /**
@@ -26,5 +27,29 @@ public class CovidApp {
         roleCnt.delete(5);
         System.out.println(result.getId());
         System.out.println(result.getName());
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new login().setVisible(true);
+            }
+        });
     }
 }
