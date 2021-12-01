7bd6ee8b8fe12d04ab919ae3c6104e072405fb02
Anderssonvla <54333121+Anderssonvla@users.noreply.github.com>
2021-11-30 21:37:45 -0600

    .

--- src/covidapp/CovidApp.java
+++ src/covidapp/CovidApp.java
@@ -45,6 +45,12 @@ public class CovidApp {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
